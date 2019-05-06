26e5e5bb404f72331cf9abc0462f04e99a872d02
Jiraffe7 <twajxjiraffe@gmail.com>
2019-05-06 22:36:10 +0800

    Native query to find attendance of member between dates

--- src/main/java/org/tpbc/tpbcapi/repository/AttendanceRepository.java
+++ src/main/java/org/tpbc/tpbcapi/repository/AttendanceRepository.java
@@ -1,9 +1,20 @@
 package org.tpbc.tpbcapi.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.tpbc.tpbcapi.entity.Attendance;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Repository
 public interface AttendanceRepository extends JpaRepository<Attendance, Long> {
+
+    @Query(
+            value = "SELECT * FROM ATTENDANCE WHERE MEMBER_ID = ?1 AND DATE >= ?2 AND DATE < ?3",
+            nativeQuery = true
+    )
+    List<Attendance> findAllByMemberBetweenDate(long userid, LocalDate lower, LocalDate upper);
+
 }

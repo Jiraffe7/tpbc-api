3eda39566404d70fa8f13520b5d92e469ed03c17
Jiraffe7 <twajxjiraffe@gmail.com>
2019-05-06 08:55:14 +0800

    Initial commit

---
+++ src/main/java/org/tpbc/tpbcapi/TpbcApiApplication.java
@@ -0,0 +1,13 @@
+package org.tpbc.tpbcapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpbcApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpbcApiApplication.class, args);
+	}
+
+}
